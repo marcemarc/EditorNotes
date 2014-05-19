5fcc71eff3ef48fea06916de947f3a013f8f2a7c
Marc Goodson <marc@snowmoon.co.uk>
2014-05-19 01:03:16 +0100

    Initial Commit

---
+++ tooorangey.EditorNotes/EditorNotes.controller.js
@@ -0,0 +1,15 @@
+﻿angular.module("umbraco")
+    .controller("tooorangey.EditorNotes",
+function ($scope) {
+    $scope.editorNotes = "<p>Have a lovely editing day!</p>";
+    $scope.noteCssClass = "";
+    if (typeof $scope.model.config.editornotes != "undefined") {
+     //angular 1.2 $scope.editornotes = $sce.trustAsHtml($scope.model.config.editornotes);
+       
+        $scope.editorNotes = $scope.model.config.editornotes;
+    }
+    if (typeof $scope.model.config.notecssclass != "undefined") {
+      $scope.noteCssClass = $scope.model.config.notecssclass;
+    }
+
+});
